58e7470816aaabe45e9d55084c663c2e2359b9e3
senabibi <c2011029@student.cankaya.edu.tr>
2024-03-27 01:46:34 +0300

    Opened new page for lab5 ,commited first; added link inside the index.html file and run the page.It is succesfully opened and run.Also i can see the Button that Hide /Show Cards.

--- RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82dc7c4fd77343d08cbd956d5841c5f703176105")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef699c92b2cb74b3381a77befdae96309efdfe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
