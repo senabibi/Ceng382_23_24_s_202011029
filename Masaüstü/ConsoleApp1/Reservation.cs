e64e07202caf6e78c1a957a408437bf7d2379cfb
senabibi <c2011029@student.cankaya.edu.tr>
2024-04-03 23:58:06 +0300

    First i worked on int types ,that was much more easy.Then my code works correctly added sttring features inside the main code file and runn the code

--- Masaüstü/ConsoleApp1/Reservation.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class Reservation
-{
-    public DateTime Time { get; set; }
-    public DateTime Date { get; set; }
-    public string ReserverName { get; set; }
-    public Room Room { get; set; }
-}
--- Masaüstü/ConsoleApp1/RoomData.cs
+++
@@ -1,7 +0,0 @@
-using System.Text.Json.Serialization;
-
-public class RoomData
-{
-    [JsonPropertyName("Room")]
-    public Room[] Rooms { get; set; }
-}
